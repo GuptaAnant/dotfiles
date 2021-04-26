8b9e287d8b91cdf9d2976c9a8d1418f24f2c2543
Anant Gupta <gupta.anant.1303@gmail.com>
2021-04-26 23:20:00 +0530

    Adding a doom emacs auto install script

--- install.sh
+++ install.sh
@@ -9,13 +9,19 @@ sudo pacman -S yay
 echo "Installed yay"
 
 echo "Now installing other dependencies"
-yay -S awesome-git alacritty brave nerd-fonts-mononoki thunar rofi fish picom starship obs-studio android-studio light-git lxsession lxappearance volumeicon feh pavucontrol libreoffice
+yay -S awesome-git alacritty brave nerd-fonts-mononoki thunar rofi fish picom starship obs-studio android-studio light-git lxsession lxappearance volumeicon feh pavucontrol libreoffice ripgrep fd emacs
+
 echo "Installed various programs that I needed"
 
 echo "Now downloading wallpaper directory"
 cd
 git clone https://gitlab.com/dwt1/wallpaper.git
 echo "Done wallpaper setup"
+
+echo "Now setting up doom emacs"
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+echo "Installed Doom Emacs"
 
 echo "Now moving folder & files"
 mv ~/dotfiles/.config/awesome ~/.config/awesome
@@ -24,6 +30,7 @@ mv ~/dotfiles/.config/rofi ~/.config/rofi/
 mv ~/dotfiles/.config/fish ~/.config/fish/
 mv ~/dotfiles/.config/picom ~/.config/picom/
 mv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+mv ~/dotfiles/.doom.d/ ~/.doom.d/
 echo "Done"
 
 echo "Now changing shell"
