982afb86641b11dc265a65c963b7ea3ea14ebc76
Anant Gupta <gupta.anant.1303@gmail.com>
2021-04-27 17:03:23 +0530

    Adding mpv & conky

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo pacman -S yay
 echo "Installed yay"
 
 echo "Now installing other dependencies"
-yay -S awesome-git alacritty brave nerd-fonts-mononoki thunar rofi exa fish picom starship obs-studio android-studio light-git lxsession lxappearance volumeicon feh pavucontrol libreoffice ripgrep fd emacs
+yay -S awesome-git alacritty brave conky mpv nerd-fonts-mononoki thunar rofi exa fish picom starship obs-studio android-studio light-git lxsession lxappearance volumeicon feh pavucontrol libreoffice ripgrep fd emacs
 
 echo "Installed various programs that I needed"
 
