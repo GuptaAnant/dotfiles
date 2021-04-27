4a236fcbf37fc56438f1ce9b0e402851ef254240
Anant Gupta <gupta.anant.1303@gmail.com>
2021-04-27 09:53:20 +0530

    Trying to remove distraction

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -8,8 +8,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Anant Gupta"
+      user-mail-address "gupta.anant.1303@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-horizon)
+(setq doom-theme 'doom-dark+)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
